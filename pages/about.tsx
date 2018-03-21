9d6dabfd60c7e81f9f9f5c8949c3505c503010da
standy <mostovoyav@gmail.com>
2018-03-21 19:46:29 +0300

    Added Button

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import {delay} from '../components/utils';
+import {Button, Divider} from 'semantic-ui-react';
+import Router from 'next/router';
 
-const A = ({delayedProps}) => (
+const A = ({delayedProps, url}) => (
 	<Layout title='ABOUT'>
 		delayedProps: {delayedProps}
+
+		<Divider />
+
+		<Button onClick={() => {
+			Router.push({
+				pathname: url.pathname,
+				query: {clicked: '1'},
+			});
+		}}>Click me</Button>
 	</Layout>
 );
 
